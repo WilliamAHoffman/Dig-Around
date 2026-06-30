46b7b40747868f5183aa3f8d47a0657625feb409
WilliamAHoffman <whoffman4230@gmail.com>
2026-06-30 18:10:22 -0400

    added deep mountains

--- Mine-Around/Assets/Scripts/World Generation/GenerationFeature.cs
+++ Mine-Around/Assets/Scripts/World Generation/GenerationFeature.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public abstract class GenerationFeature : ScriptableObject
 {
-    public WorldSample idealSample;
+    public TargetWorldSample idealSample;
 
     [Range(0f, 1f)]
     public float minSimilarity = 0.5f;
-
     public abstract GenerationResult Apply(
         Vector2Int location,
         float strength,
@@ -18,22 +17,29 @@ public abstract class GenerationFeature : ScriptableObject
         if (idealSample == null || idealSample.keys == null || idealSample.keys.Count == 0)
             return 0f;
 
-        float totalDifference = 0f;
-        int count = 0;
+        float weightedDifference = 0f;
+        float totalImportance = 0f;
 
         foreach (WorldSampleType type in idealSample.keys)
         {
-            float idealValue = idealSample.Get(type);
-            float worldValue = worldSample.Get(type);
+            float importance = idealSample.GetImportance(type);
 
-            totalDifference += Mathf.Abs(idealValue - worldValue);
-            count++;
+            if (importance <= 0f)
+                continue;
+
+            float idealValue = idealSample.GetValue(type);
+            float worldValue = worldSample.GetValue(type);
+
+            float difference = Mathf.Abs(idealValue - worldValue);
+
+            weightedDifference += difference * importance;
+            totalImportance += importance;
         }
 
-        if (count == 0)
+        if (totalImportance <= 0f)
             return 0f;
 
-        float averageDifference = totalDifference / count;
+        float averageDifference = weightedDifference / totalImportance;
 
         return 1f - Mathf.Clamp01(averageDifference);
     }
--- Mine-Around/Assets/Scripts/World Generation/NoiseSample.cs
+++ Mine-Around/Assets/Scripts/World Generation/NoiseSample.cs
@@ -19,7 +19,7 @@ public class WorldSample
         values.Add(value);
     }
 
-    public float Get(WorldSampleType type)
+    public float GetValue(WorldSampleType type)
     {
         if (!keys.Contains(type))
         {
@@ -27,5 +27,40 @@ public class WorldSample
             return 0;
         }
         return values[keys.IndexOf(type)];
+    }
+}
+
+[Serializable]
+public class TargetWorldSample
+{
+    [SerializeField] public List<WorldSampleType> keys = new List<WorldSampleType>();
+    [SerializeField] public List<float> values = new List<float>();
+    [SerializeField] public List<float> importance = new List<float>();
+
+    public void Add(WorldSampleType type, float value, float influence)
+    {
+        keys.Add(type);
+        values.Add(value);
+        importance.Add(influence);
+    }
+
+    public float GetValue(WorldSampleType type)
+    {
+        if (!keys.Contains(type))
+        {
+            Debug.LogError("World sample does not contain this type");
+            return 0;
+        }
+        return values[keys.IndexOf(type)];
+    }
+
+    public float GetImportance(WorldSampleType type)
+    {
+        if (!keys.Contains(type))
+        {
+            Debug.LogError("World sample does not contain this type");
+            return 0;
+        }
+        return importance[keys.IndexOf(type)];
     }
 }
